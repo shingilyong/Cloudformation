8823e8c6cb3ea2fb2b34fc6d24f053d94b714e46
unknown <sgy12303@gmail.com>
2021-03-31 02:20:15 +0900

    2021-03-31

---
+++ 3tier-web-application/DB/main.tf
@@ -0,0 +1,23 @@
+# Subnet group 생성
+resource "aws_db_subnet_group" "terraform-subnet-group" {
+  name       = "terraform-subnet-group"
+  subnet_ids = [aws_subnet.terraform-db-1.id, aws_subnet.terraform-db-2.id]
+
+  tags = {
+    Name = "terraform-subnet-group"
+  }
+}
+
+# RDS DB Instance 생성
+resource "aws_db_instance" "terraform-DB" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "8.0.20"
+  instance_class       = "db.t2.micro"
+  name                 = "terraformDB"
+  username             = "admin"
+  password             = "qwer1234"
+  skip_final_snapshot  = true
+  db_subnet_group_name  = "terraform-subnet-group"
+  vpc_security_group_ids = ["${aws_security_group.terraform-db-sg.id}"]
+  }
